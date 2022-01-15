b48012460b5e5d0952c327e62eeb12828b8e9748
Christoph Wagner <christoph.wagner@tu-ilmenau.de>
2022-01-15 19:21:35 +0100

    Provide .tsx tile files

---
+++ MapUtilities.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="MapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="mapUtilities.png" width="306" height="34"/>
+</tileset>
