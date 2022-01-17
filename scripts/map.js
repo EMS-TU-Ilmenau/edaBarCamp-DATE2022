53f5a4dba3fe4b5a8f58901596a4b760884cb05a
georg-imms <92916610+georg-imms@users.noreply.github.com>
2022-01-17 15:55:00 +0100

    Add postboxes and session card templates

---
+++ scripts/map.js
@@ -0,0 +1,29 @@
+
+// Messages for proposal cards
+WA.room.onEnterLayer('topicSubmission').subscribe(() => {
+    const triggerMessage = WA.ui.displayActionMessage({
+        message: "Press Space or click here to open topic submission website! \n (Please use the template at the signs)",
+        callback: () => {
+            WA.nav.openTab("https://cloud.tu-ilmenau.de/s/2qjgze9AopATe7M");
+        }
+    });
+
+    setTimeout(() => {
+        // later
+        triggerMessage.remove();
+    }, 20000);
+})
+
+WA.room.onEnterLayer('proposalCardDownload').subscribe(() => {
+    const triggerMessage = WA.ui.displayActionMessage({
+        message: "Press Space or click here to download the proposal card template! \n (Upload your proposals at the post boxes)",
+        callback: () => {
+            WA.nav.openTab("https://cloud.tu-ilmenau.de/s/kqZeDeLcToigTxr");
+        }
+    });
+
+    setTimeout(() => {
+        // later
+        triggerMessage.remove();
+    }, 20000);
+})
