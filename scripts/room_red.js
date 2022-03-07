a5736c3d2ccb64cd6034d138a911230251715929
georg-imms <92916610+georg-imms@users.noreply.github.com>
2022-03-07 21:47:44 +0100

    Update rooms
    remove miro-iframe
    add popup for miro
    expand jitsi area in miro rooms
    use wider doors

---
+++ scripts/room_red.js
@@ -0,0 +1,13 @@
+WA.room.onEnterLayer('openBoard').subscribe(() => {
+    const triggerMessage = WA.ui.displayActionMessage({
+        message: "Press Space or click here to open the board for this session!",
+        callback: () => {
+            WA.nav.openTab("https://ems-tu-ilmenau.github.io/edaBarCamp-DATE2022/miro-boards/html/red_room.html");
+        }
+    });
+
+    setTimeout(() => {
+        // later
+        triggerMessage.remove();
+    }, 10000);
+})
