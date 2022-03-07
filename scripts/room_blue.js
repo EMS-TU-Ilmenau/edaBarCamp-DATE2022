953793f172313dac37d39288800df631a1098e98
georg-imms <92916610+georg-imms@users.noreply.github.com>
2022-03-07 21:03:33 +0100

    update blue room

--- scripts/room_blue.js
+++ scripts/room_blue.js
@@ -1,4 +1,4 @@
-WA.room.onEnterLayer('miroBoard').subscribe(() => {
+WA.room.onEnterLayer('openBoard').subscribe(() => {
     const triggerMessage = WA.ui.displayActionMessage({
         message: "Press Space or click here to open topic submission website! \n (Please use the template at the signs)",
         callback: () => {
