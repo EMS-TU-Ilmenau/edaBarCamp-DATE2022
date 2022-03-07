dd883cd5518156de733d7175e50be039f8f27cb0
georg-imms <92916610+georg-imms@users.noreply.github.com>
2022-03-07 20:46:45 +0100

    Update arena.js

--- scripts/arena.js
+++ scripts/arena.js
@@ -1,7 +1,14 @@
 
+triggerMessage = null;
+
 // Messages for proposal cards
 WA.room.onEnterLayer('topicSubmission').subscribe(() => {
-    const triggerMessage = WA.ui.displayActionMessage({
+    if (triggerMessage != null) {
+        triggerMessage.remove();
+    }
+
+
+    triggerMessage = WA.ui.displayActionMessage({
         message: "Press Space or click here to open topic submission website! \n (Please use the template at the signs)",
         callback: () => {
             WA.nav.openTab("https://cloud.tu-ilmenau.de/s/2qjgze9AopATe7M");
@@ -15,7 +22,11 @@ WA.room.onEnterLayer('topicSubmission').subscribe(() => {
 })
 
 WA.room.onEnterLayer('proposalCardDownload').subscribe(() => {
-    const triggerMessage = WA.ui.displayActionMessage({
+    if (triggerMessage != null) {
+        triggerMessage.remove();
+    }
+
+    triggerMessage = WA.ui.displayActionMessage({
         message: "Press Space or click here to download the proposal card template! \n (Upload your proposals at the post boxes)",
         callback: () => {
             WA.nav.openTab("https://cloud.tu-ilmenau.de/s/kqZeDeLcToigTxr");
